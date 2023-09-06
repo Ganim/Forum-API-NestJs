51781ba1628269e3560e18f2ea63670a4665c3ec
Ganim <guilhermeganim@hotmail.com>
2023-09-06 11:09:51 -0300

    Add and Config Vitest

---
+++ vitest.config.ts
@@ -0,0 +1,22 @@
+/*
+  Instalar os seguintes módulos:
+    pnpm i unplugin-swc @swc/core @vitest/coverage-v8 -D
+    pnpm i vite-tsconfig-paths -D
+
+*/
+import swc from 'unplugin-swc'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    globals: true,
+    root: './',
+  },
+  plugins: [
+    // This is required to build the test files with SWC
+    swc.vite({
+      // Explicitly set the module type to avoid inheriting this value from a `.swcrc` config file
+      module: { type: 'es6' },
+    }),
+  ],
+})
