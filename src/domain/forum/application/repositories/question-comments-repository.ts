88b7b9cbafb6dc79766d9e50170cd01c18da1e16
Ganim <guilhermeganim@hotmail.com>
2023-10-02 22:37:46 -0300

    add comments with author on prisma

--- src/domain/forum/application/repositories/question-comments-repository.ts
+++ src/domain/forum/application/repositories/question-comments-repository.ts
@@ -1,12 +1,19 @@
 import { PaginationParams } from '@/core/repositories/pagination-params'
 import { QuestionComment } from '../../enterprise/entities/question-comment'
+import { CommentWithAuthor } from '../../enterprise/entities/value-objects/comment-with-author'
 
 export abstract class QuestionCommentsRepository {
   abstract findById(id: string): Promise<QuestionComment | null>
+
   abstract findManyByQuestionId(
     questionId: string,
     parms: PaginationParams,
   ): Promise<QuestionComment[]>
+
+  abstract findManyByQuestionIdWithAuthor(
+    questionId: string,
+    parms: PaginationParams,
+  ): Promise<CommentWithAuthor[]>
 
   abstract create(questionComment: QuestionComment): Promise<void>
   abstract delete(questionComment: QuestionComment): Promise<void>
